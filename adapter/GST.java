ea218a71d55892fcf4042b237c4b14ebaa4c1172
Manav R <67834464+MXNXV-ERR@users.noreply.github.com>
2023-04-27 19:10:37 +0000

    tada

---
+++ adapter/GST.java
@@ -0,0 +1,12 @@
+package adapter;
+
+public class GST implements CalcTax {
+
+	@Override
+	public float taxAmount(int qty, float price) {
+		// TODO Auto-generated method stub
+		return qty*price*0.18f;
+	}
+
+}
+
